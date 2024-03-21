956eb6978aea9f2e58e6c82b91851338bd18bd63
Tannyson Lucas <tannysonmartins@gmail.com>
2024-03-21 19:55:45 -0300

    Creating and enhancing the ChessPosition and ChessException class

--- SystemXadrezGamer/src/chess/ChessMatch.java
+++ SystemXadrezGamer/src/chess/ChessMatch.java
@@ -1,47 +1,54 @@
 package chess;
 
 import boardgamer.Board;
-import boardgamer.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
- //Importando o BOARD
+//Importando o BOARD
 public class ChessMatch {
-	//Importando o BOARD
-	  private Board board;
-	  /*
-	  private int turn;
-	  private Color currentPlay;
-	  private boolean check;
-	  private boolean checkMate;
-	  private ChessPiece enPassantVulnerable;
-	  private ChessPiece promoted;
-	  */
-//Construtor para designar o tamanho do tabuleiro
-	  public ChessMatch() {
-		  board = new Board(8, 8);
-		  initialSetup();
-	  }
- //Metodo que ira retornar a matriz de pieces(peças) correspondente a ChessMatch
-//Vamos liberar apenas a camada de peças e não de tabuleiro para o programa reconhecer	  
-	  public ChessPiece[][] getPieces(){
-		  //Criando variavel temporaria e matriz que ira receber a linha e coluna
-		  ChessPiece[][] mat = new ChessPiece[board.getRow()][board.getColumn()];
-		  //Construiremos um sistema de repetição que irá percorrer as linhas e colunas da matriz
-		  for(int i=0; i<board.getRow();i++) {
-			  for(int j=0; j<board.getColumn(); j++) {
-				  //para cada posição da "i""j" do meu tabuleiro receba... 
-				  //Porem, precisamos fazer um DOWNCATCHS para interpretar como uma peça de xadrez
-				  mat[i][j] = (ChessPiece)board.piece(i, j);
-			  }
-		  }
-		  return mat;
-	  } 
-	  //Metodo responsavel por iniciar a partida de xadrez
-	  private void initialSetup() {
-		  board.PlacePiece(new Rook(board, Color.WHITE), new Position(2,1));
-		  board.PlacePiece(new King(board, Color.BLACK), new Position(2,4));
-		  board.PlacePiece(new King(board, Color.WHITE), new Position(7,4));
+	// Importando o BOARD
+	private Board board;
 
-	  }
+	/*
+	 * private int turn; private Color currentPlay; private boolean check; private
+	 * boolean checkMate; private ChessPiece enPassantVulnerable; private ChessPiece
+	 * promoted;
+	 */
+//Construtor para designar o tamanho do tabuleiro
+	public ChessMatch() {
+		board = new Board(8, 8);
+		initialSetup();
+	}
+
+	// Metodo que ira retornar a matriz de pieces(peças) correspondente a ChessMatch
+//Vamos liberar apenas a camada de peças e não de tabuleiro para o programa reconhecer	  
+	public ChessPiece[][] getPieces() {
+		// Criando variavel temporaria e matriz que ira receber a linha e coluna
+		ChessPiece[][] mat = new ChessPiece[board.getRow()][board.getColumn()];
+		// Construiremos um sistema de repetição que irá percorrer as linhas e colunas
+		// da matriz
+		for (int i = 0; i < board.getRow(); i++) {
+			for (int j = 0; j < board.getColumn(); j++) {
+				// para cada posição da "i""j" do meu tabuleiro receba...
+				// Porem, precisamos fazer um DOWNCATCHS para interpretar como uma peça de
+				// xadrez
+				mat[i][j] = (ChessPiece) board.piece(i, j);
+			}
+		}
+		return mat;
+	}
+	// Metodo to toPosition que irá instanciar as peças de xadrez informando a
+	// coordenada
+	// no sistema do xadrez
+
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.PlacePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+
+	// Metodo responsavel por iniciar a partida de xadrez
+	private void initialSetup() {
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
+	}
 }
---
+++ SystemXadrezGamer/src/chess/ChessPosition.java
@@ -0,0 +1,43 @@
+package chess;
+
+import boardgamer.Position;
+
+public class ChessPosition {
+
+	private char column;
+	private int row;
+
+	public ChessPosition(char column, int row) {
+		//Programação Defensiva
+		if ( column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 at h8");
+		}
+		this.column = column;
+		this.row = row;
+
+	}
+
+	public char getColumn() {
+		return column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	//metodo para organizar a posição... "#" significa que o metodo/variavel... são Protected
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
+	}
+	//Quando um metodo estiver sublinhado, significa que ele é "static"
+	protected static ChessPosition fromPosition(Position position) {
+		//Precisamos sempre converter para Char, porque ela não é automatica no java
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+	}
+
+	@Override
+	public String toString() {
+		return "" + column + row;
+		//O espaço vazio e um macete para fazer o compilador aceitar o comando
+	}
+}
