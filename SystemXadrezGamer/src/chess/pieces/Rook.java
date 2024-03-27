3ce748e2b5df95d8db24d1022a212b9a00de2549
Tannyson Lucas <tannysonmartins@gmail.com>
2024-03-27 19:45:44 -0300

    Implemeting possible moves of King

--- SystemXadrezGamer/src/chess/pieces/Rook.java
+++ SystemXadrezGamer/src/chess/pieces/Rook.java
@@ -5,9 +5,10 @@ import boardgamer.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rook extends ChessPiece{
+public class Rook extends ChessPiece {
 
-	//Está informando o tabuleiro e a cor da peça do tabuleiro e repassa para o construtor da superClass
+	// Está informando o tabuleiro e a cor da peça do tabuleiro e repassa para o
+	// construtor da superClass
 	public Rook(Board board, Color color) {
 		super(board, color);
 		// TODO Auto-generated constructor stub
@@ -21,62 +22,65 @@ public class Rook extends ChessPiece{
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRow()][getBoard().getColumn()];
-		
-		Position p = new Position(0,0);
-		
-		//above
-		//Verifica a posição da peça e demanda o tipo de movimento que ela poderá fazer
-		p.setValues(position.getRow() -1, position.getColumn());
-		//Testando a existencia de uma peça e se na rota de locomação dela, não tem outra peça
+
+		Position p = new Position(0, 0);
+
+		// above
+		// Verifica a posição da peça e demanda o tipo de movimento que ela poderá fazer
+		p.setValues(position.getRow() - 1, position.getColumn());
+		// Testando a existencia de uma peça e se na rota de locomação dela, não tem
+		// outra peça
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setRow(p.getRow() - 1);
-		}//Verificar se tem uma peça adversaria na rota de movimento e validando ela como true
-		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-		
-		//left
-		// Verifica a posição da peça e demanda o tipo de movimento que ela poderá fazer
-		p.setValues(position.getRow(), position.getColumn() - 1);
-		// Testando a existencia de uma peça e se na rota de locomação dela, não tem
-		// outra peça
-		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() - 1);
-		} // Verificar se tem uma peça adversaria na rota de movimento e validando ela
+			// Verificar se tem uma peça adversaria na rota de movimento e validando ela
 			// como true
-		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
 		}
-		
-		//right
-		// Verifica a posição da peça e demanda o tipo de movimento que ela poderá fazer
-		p.setValues(position.getRow(), position.getColumn() + 1);
-		// Testando a existencia de uma peça e se na rota de locomação dela, não tem
-		// outra peça
-		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() + 1);
-		} // Verificar se tem uma peça adversaria na rota de movimento e validando ela
-			// como true
-		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-		
-		//below
-		// Verifica a posição da peça e demanda o tipo de movimento que ela poderá fazer
-		p.setValues(position.getRow() + 1, position.getColumn());
-		// Testando a existencia de uma peça e se na rota de locomação dela, não tem
-		// outra peça
-		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() + 1);
-		} // Verificar se tem uma peça adversaria na rota de movimento e validando ela
-			// como true
-		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
+			// left
+			// Verifica a posição da peça e demanda o tipo de movimento que ela poderá fazer
+			p.setValues(position.getRow(), position.getColumn() - 1);
+			// Testando a existencia de uma peça e se na rota de locomação dela, não tem
+			// outra peça
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				p.setColumn(p.getColumn() - 1);
+			} // Verificar se tem uma peça adversaria na rota de movimento e validando ela
+				// como true
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+
+			// right
+			// Verifica a posição da peça e demanda o tipo de movimento que ela poderá fazer
+			p.setValues(position.getRow(), position.getColumn() + 1);
+			// Testando a existencia de uma peça e se na rota de locomação dela, não tem
+			// outra peça
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				p.setColumn(p.getColumn() + 1);
+			} // Verificar se tem uma peça adversaria na rota de movimento e validando ela
+				// como true
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+
+			// below
+			// Verifica a posição da peça e demanda o tipo de movimento que ela poderá fazer
+			p.setValues(position.getRow() + 1, position.getColumn());
+			// Testando a existencia de uma peça e se na rota de locomação dela, não tem
+			// outra peça
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				p.setRow(p.getRow() + 1);
+			} // Verificar se tem uma peça adversaria na rota de movimento e validando ela
+				// como true
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
 		return mat;
+
 	}
 }
